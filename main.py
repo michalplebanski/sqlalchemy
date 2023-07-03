ca9b5be8d58b92c7af134d5e81766557a4dc8d79
Michał Plebański <michalplebanski@gmail.com>
2023-07-03 15:16:42 +0200

    add CSV files and update code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Table, Column, Date, Float, Integer, Text, MetaData
 from sqlalchemy import create_engine
-
+import csv
 
 clean_db = create_engine('sqlite:///clean.db')
 
@@ -32,36 +32,45 @@ meta.create_all(clean_db)
 #insert data to database
 #clean_measure
 
-ins_measure = clean_measure.insert()
 conn = clean_db.connect()
-conn.execute(ins_measure, [
-   {'station': 'USC00519397', 'date': '2010-01-01', 'precib': '0.08', 'tobs': '65'},
-   {'station': 'USC00519397', 'date': '2010-01-02', 'precib': '0.0', 'tobs': '63'},
-   {'station': 'USC00519397', 'date': '2010-01-03', 'precib': '0.0', 'tobs': '74'},
-   {'station': 'USC00519397', 'date': '2010-01-04', 'precib': '0.0', 'tobs': '76'},
-   {'station': 'USC00519397', 'date': '2010-01-06', 'precib': '0.0', 'tobs': '73'},
-   {'station': 'USC00519397', 'date': '2010-01-07', 'precib': '0.06', 'tobs': '70'},
-   {'station': 'USC00519397', 'date': '2010-01-09', 'precib': '0.0', 'tobs': '68'},
-   {'station': 'USC00519397', 'date': '2010-01-10', 'precib': '0.0', 'tobs': '54'}
-])
+
+with open('clean_measure.csv', 'r') as clean_measure_file:
+    csvreader = csv.reader(clean_measure_file)
+    
+    # Skip header if exists
+    next(csvreader)
+    
+    # Iterate through all rows in CSV file and add them to database
+    for row in csvreader:
+        station = row[0]
+        date = row[1]
+        precib = float(row[2])
+        tobs = int(row[3])
+
+        conn.execute('INSERT INTO clean_measure VALUES (?, ?, ?, ?)', (station, date, precib, tobs))
 
 #clean_station
 
-ins_stations = clean_stations.insert()
 conn = clean_db.connect()
-conn.execute(ins_stations, [
-   {'station': 'USC00519397', 'latitude': '21.2716', 'longitude': '-157.8168', 'elevation': '3.0', 'name': 'WAIKIKI 717.2', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00513117', 'latitude': '21.4234', 'longitude': '-157.8015', 'elevation': '14.6', 'name': 'KANEOHE 838.1', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00514830', 'latitude': '21.5213', 'longitude': '-157.8374', 'elevation': '7.0', 'name': 'KUALOA RANCH HEADQUARTERS 886.9', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00517948', 'latitude': '21.3934', 'longitude': '-157.9751', 'elevation': '11.9', 'name': 'PEARL CITY', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00518838', 'latitude': '21.4992', 'longitude': '-158.0111', 'elevation': '306.6', 'name': 'UPPER WAHIAWA 874.3', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00519523', 'latitude': '21.33556', 'longitude': '-157.71139', 'elevation': '19.5', 'name': 'WAIMANALO EXPERIMENTAL FARM', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00519281', 'latitude': '21.45167', 'longitude': '-157.84888999999998', 'elevation': '32.9', 'name': 'WAIHEE 837.5', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00511918', 'latitude': '21.3152', 'longitude': '-157.9992', 'elevation': '0.9', 'name': 'HONOLULU OBSERVATORY 702.2', 'country': 'US', 'state': 'HI'},
-   {'station': 'USC00516128', 'latitude': '21.3331', 'longitude': '-157.8025', 'elevation': '152.4', 'name': 'MANOA LYON ARBO 785.2', 'country': 'US', 'state': 'HI'}
-])
 
-print(conn.execute("SELECT * FROM clean_measure LIMIT 5").fetchall())
-print(conn.execute("SELECT * FROM clean_stations LIMIT 5").fetchall())
+with open('clean_stations.csv', 'r') as clean_stations_file:
+    csvreader = csv.reader(clean_stations_file)
+    
+    # Skip header if exists
+    next(csvreader)
+    
+    # Iterate through all rows in CSV file and add them to database
+    for row in csvreader:
+        station = row[0]
+        latitude = float(row[1])
+        longitude = float(row[2])
+        elevation = float(row[3])
+        name = row[4]
+        country = row[5]
+        state = row[6]
 
+        conn.execute('INSERT INTO clean_stations VALUES (?, ?, ?, ?, ?, ?, ?)', (station, latitude, longitude, elevation, name, country, state))
 
+"""print(conn.execute("SELECT * FROM clean_measure LIMIT 5").fetchall())
+print(conn.execute("SELECT * FROM clean_stations LIMIT 5").fetchall()) 
+"""
